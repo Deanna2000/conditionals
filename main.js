96342189b23889e0a1a2d6e40b823828c768740c
Deanna Vickers <deanna2000@gmail.com>
2018-04-03 13:45:25 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// Exercise: Using Javascript, create a variable to hold your last name. Using the if-else
+// statement, do the following: If your last name has 7 or more letters, console log "I can't
+// pronounce this last name". Else, console log your last name.
+
+
+
+function lastNameResponder (){
+    const lastName = "Vickers";
+
+    if (lastName.length > 6) {
+        console.log(lastName.length);
+        console.log("I can't pronounce this last name.");
+
+    }
+    else {
+        console.log(lastName);
+}
+}
+function lastNameResponder();
